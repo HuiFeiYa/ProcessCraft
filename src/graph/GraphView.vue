bbfaa452feef979ab574cda034d10f368421f227
guanwanxiao <18257132004@163.com>
2024-01-21 11:45:44 +0800

    feat：快速创建 icon

--- src/graph/GraphView.vue
+++ src/graph/GraphView.vue
@@ -1,70 +1,87 @@
 <script setup lang="ts">
-import { provide, computed, onMounted, ref } from 'vue'
-import DiagramShape from './DiagramShape.vue'
-import { GraphModel } from './models/graphModel';
-import { shapeComps } from './shape/index'
-import ShapeMovePreview from './shape/interaction/ShapeMovePreview.vue'
-import SelectionVertex from './shape/interaction/SelectionVertex.vue';
-import ShapeResizePreview from './shape/interaction/ShapeResizePreview.vue';
-import EdgeMovePreview from './shape/interaction/EdgeMovePreview.vue'
-import Markers from './shape/interaction/Markers.vue'
-import { EventType, VertexType } from './shape/constant';
-import { ShapeType } from './types';
-import { useDrawStore } from '../editor/store';
-const store = useDrawStore()
-const props = defineProps<{ graph: GraphModel }>()
-provide('graph', props.graph)
-props.graph.registerShapeComps(shapeComps)
+import { provide, computed, onMounted, ref } from "vue";
+import DiagramShape from "./DiagramShape.vue";
+import { GraphModel } from "./models/graphModel";
+import { shapeComps } from "./shape/index";
+import ShapeMovePreview from "./shape/interaction/ShapeMovePreview.vue";
+import SelectionVertex from "./shape/interaction/SelectionVertex.vue";
+import ShapeResizePreview from "./shape/interaction/ShapeResizePreview.vue";
+import EdgeMovePreview from "./shape/interaction/EdgeMovePreview.vue";
+import Markers from "./shape/interaction/Markers.vue";
+import { EventType, VertexType } from "./shape/constant";
+import { ShapeType } from "./types";
+import { useDrawStore } from "../editor/store";
+import QuickCreatePoint from "./shape/interaction/QuickCreatePoint.vue";
+import { SubShapeType } from "./types/index";
+const store = useDrawStore();
+const props = defineProps<{ graph: GraphModel }>();
+provide("graph", props.graph);
+props.graph.registerShapeComps(shapeComps);
 const viewDom = ref<HTMLDivElement | null>(null);
 onMounted(() => {
   if (!viewDom.value) return;
   props.graph.viewModel.setViewDom(viewDom.value);
-})
+});
 /**
  * store 中是最新的样式， model 中存储的是历史的，@todo，如何同步？
  */
 const selectedShapes = computed(() => {
-  const ids = props.graph.selectionModel.selectedShapes.map(shape => shape.id)
-  return store.shapes.filter(shape => ids.includes(shape.id))
-})
+  const ids = props.graph.selectionModel.selectedShapes.map(
+    (shape) => shape.id
+  );
+  return store.shapes.filter((shape) => ids.includes(shape.id));
+});
 const showSelectionVertex = computed(() => {
-  return selectedShapes.value.length > 0 && !props.graph.moveModel.showMovingPreview && !props.graph.resizeModel.showResizePreview && !props.graph.edgePointMoveModel.showPreview
-})
+  return (
+    selectedShapes.value.length > 0 &&
+    !props.graph.moveModel.showMovingPreview &&
+    !props.graph.resizeModel.showResizePreview &&
+    !props.graph.edgePointMoveModel.showPreview
+  );
+});
+
+const showQuickCreatePoint = computed(() => {
+  if (
+    selectedShapes.value.length === 1 &&
+    selectedShapes.value[0].subShapeType === SubShapeType.Block
+  ) {
+    return true;
+  }
+  return false;
+});
+
 function handleVertexMousedown(event: MouseEvent, index: VertexType) {
-  const graph = props.graph
+  const graph = props.graph;
   if (graph.selectionModel.selection.length === 0) return;
   if (graph.selectionModel.selection.length > 1) {
     graph.selectionModel.clearSelection();
   } else {
     const targetShape = selectedShapes.value[0];
     if (targetShape.shapeType === ShapeType.Symbol) {
-      graph.resizeModel.startResize(event, selectedShapes.value[0], index)
+      graph.resizeModel.startResize(event, selectedShapes.value[0], index);
     } else if (targetShape.shapeType === ShapeType.Edge) {
-      graph.edgePointMoveModel.onEdgePointMouseDown(event, targetShape, index)
+      graph.edgePointMoveModel.onEdgePointMouseDown(event, targetShape, index);
     }
   }
 }
-function handleClickOut() {
-
-
-}
+function handleClickOut() {}
 // 监听画布上点击事件，用于清空选中状态
 function handleMousedownOut(event: MouseEvent) {
   props.graph.emitter.emit(EventType.SHAPE_MOUSE_DOWN, event, undefined);
 }
 function handleMouseupOut() {
-  props.graph.emitter.emit(EventType.SHAPE_MOUSE_UP, window.event, props.graph.rootShape);
+  props.graph.emitter.emit(
+    EventType.SHAPE_MOUSE_UP,
+    window.event,
+    props.graph.rootShape
+  );
 }
 function handleMousemoveOut(event: MouseEvent) {
   props.graph.emitter.emit(EventType.SHAPE_MOUSE_MOVE, event, undefined);
 }
-function handleDragOver() {
+function handleDragOver() {}
 
-}
-
-const handleDrop = () => {
-
-};
+const handleDrop = () => {};
 </script>
 <template>
   <div class="graph-view" ref="viewDom">
@@ -72,11 +89,21 @@ const handleDrop = () => {
           展示层
           * 整个画布的事件监听
         -->
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" transform-origin="0 0"
-      style="min-width: 100%; min-height: 100%;" :width="graph.viewModel.bounds.width"
-      :height="graph.viewModel.bounds.height" @click="handleClickOut" @mousedown="handleMousedownOut"
-      @mouseup="handleMouseupOut" @mousemove="handleMousemoveOut" @dragover="handleDragOver" @drop.stop="handleDrop">
-      <g style="width: 100%;height: 100%;background-color: white;">
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      transform-origin="0 0"
+      style="min-width: 100%; min-height: 100%"
+      :width="graph.viewModel.bounds.width"
+      :height="graph.viewModel.bounds.height"
+      @click="handleClickOut"
+      @mousedown="handleMousedownOut"
+      @mouseup="handleMouseupOut"
+      @mousemove="handleMousemoveOut"
+      @dragover="handleDragOver"
+      @drop.stop="handleDrop"
+    >
+      <g style="width: 100%; height: 100%; background-color: white">
         <DiagramShape :graph="graph" :shape="graph.rootShape" />
       </g>
     </svg>
@@ -84,21 +111,53 @@ const handleDrop = () => {
       交互层
       * 设置了pointer-events， 元素永远不会成为鼠标事件的target (en-US)。其后端元素可以捕获
      -->
-    <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
-      style="min-width: 100%; min-height: 100%; position: absolute; top: 0; left: 0; pointer-events: none"
-      transform-origin="0 0" :width="graph.viewModel.bounds.width" :height="graph.viewModel.bounds.height">
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      style="
+        min-width: 100%;
+        min-height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        pointer-events: none;
+      "
+      transform-origin="0 0"
+      :width="graph.viewModel.bounds.width"
+      :height="graph.viewModel.bounds.height"
+    >
       <g>
-        <Shape-move-preview v-if="graph.moveModel.showMovingPreview" :shapes="graph.moveModel.movingShapes"
-          :dx="graph.moveModel.previewDx" :dy="graph.moveModel.previewDy" />
-        <Shape-resize-preview v-if="graph.resizeModel.showResizePreview" :bounds="graph.resizeModel.previewBounds" />
-        <selection-vertex v-if="showSelectionVertex" :selection="selectedShapes" :resize-model="graph.resizeModel"
-          @vertex-mousedown="handleVertexMousedown" />
-        <edge-move-preview v-if="graph.edgePointMoveModel.showPreview" :shape="graph.edgePointMoveModel.movingShape"
-          :previewPath="graph.edgePointMoveModel.previewPath" />
-        <Markers v-if="graph.markerModel.markerMap.size" :markerMap="graph.markerModel.markerMap" />
+        <Shape-move-preview
+          v-if="graph.moveModel.showMovingPreview"
+          :shapes="graph.moveModel.movingShapes"
+          :dx="graph.moveModel.previewDx"
+          :dy="graph.moveModel.previewDy"
+        />
+        <Shape-resize-preview
+          v-if="graph.resizeModel.showResizePreview"
+          :bounds="graph.resizeModel.previewBounds"
+        />
+        <selection-vertex
+          v-if="showSelectionVertex"
+          :selection="selectedShapes"
+          :resize-model="graph.resizeModel"
+          @vertex-mousedown="handleVertexMousedown"
+        />
+        <edge-move-preview
+          v-if="graph.edgePointMoveModel.showPreview"
+          :shape="graph.edgePointMoveModel.movingShape"
+          :previewPath="graph.edgePointMoveModel.previewPath"
+        />
+        <Markers
+          v-if="graph.markerModel.markerMap.size"
+          :markerMap="graph.markerModel.markerMap"
+        />
         <!-- <LabelEditor v-if="graph.labelEditorModel.showPreview" :editor-model="graph.labelEditorModel" /> -->
+        <QuickCreatePoint
+          v-if="showQuickCreatePoint"
+          :bounds="selectedShapes[0].bounds"
+        ></QuickCreatePoint>
       </g>
-
     </svg>
   </div>
 </template>
---
+++ src/graph/shape/interaction/QuickCreatePoint.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { Bounds } from "../../types";
+
+const props = defineProps<{ bounds: Bounds }>();
+const handleCreate = () => {};
+/** 控制点宽度 */
+const width = 16;
+const height = 16;
+const space = 5;
+</script>
+<template>
+  <g style="pointer-events: all">
+    <!-- 创建点 -->
+    <!-- 上 -->
+    <foreignObject
+      :x="bounds.absX + bounds.width / 2"
+      :y="bounds.absY"
+      :width="width"
+      :height="height"
+      :style="{ transform: `translate(-${width / 2}px, ${-height - space}px)` }"
+      @mousedown="handleCreate"
+    >
+      <el-icon class="create"><CirclePlus /></el-icon>
+    </foreignObject>
+
+    <!-- 下 -->
+    <foreignObject
+      :x="bounds.absX + bounds.width / 2"
+      :y="bounds.absY + bounds.height"
+      :width="width"
+      :height="height"
+      fill="#000"
+      :style="{ transform: `translate(-${width / 2}px, ${space}px` }"
+      @mousedown="handleCreate"
+    >
+      <el-icon class="create"><CirclePlus /></el-icon>
+    </foreignObject>
+    <!-- 右 -->
+    <foreignObject
+      :x="bounds.absX + bounds.width"
+      :y="bounds.absY + bounds.height / 2"
+      :width="width"
+      :height="height"
+      fill="#000"
+      :style="{ transform: `translate( ${width / 2}px,-${height / 2}px)` }"
+      @mousedown="handleCreate"
+    >
+      <el-icon class="create"><CirclePlus /></el-icon>
+    </foreignObject>
+    <!-- 左 -->
+    <foreignObject
+      :x="bounds.absX"
+      :y="bounds.absY + bounds.height / 2"
+      :width="width"
+      :height="height"
+      fill="#000"
+      :style="{ transform: `translate(-${width + space}px,-${height / 2}px)` }"
+      @mousedown="handleCreate"
+    >
+      <el-icon class="create"><CirclePlus /></el-icon>
+    </foreignObject>
+  </g>
+</template>
+<style>
+.create {
+  color: #7ebfde;
+  font-weight: 500;
+}
+.create:hover {
+  color: #0795d6;
+}
+</style>
